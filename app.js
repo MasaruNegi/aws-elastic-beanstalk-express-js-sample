ffce0545ab74b5ddc80f20a3cbbcd17cce7278c8
根木 優 <0000694@dkgroup.local>
2022-07-09 09:23:36 +0900

    Full pipeline bug test4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CodePipeLine BUG Test!)));
+app.get('/', (req, res) => res.send('CodePipeLine BUG Test!));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
